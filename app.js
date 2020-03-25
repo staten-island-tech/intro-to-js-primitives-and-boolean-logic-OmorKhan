763d8d69d5a5ba15187b58486f34281e38e82448
OmorKhan <56314605+OmorKhan@users.noreply.github.com>
2020-03-25 10:18:15 -0400

    ju

---
+++ app.js
@@ -0,0 +1,74 @@
+console.log("connected");
+
+
+
+// let string = "i am a string";
+// let sentence = "Hello Angle " + "And Vicky";
+// let combine = string + sentence
+
+// let student = "Sandra";
+// let student2 = "Gia";
+// let courseName = "Ap CSP";
+// let teacherName = "Whalen";
+//      student +
+//      " and " +
+//      student2 +
+//      " are taking " +
+//      courseName +
+//      " with " +
+//      teacherName;
+
+//////////number
+
+// let year = 2020;
+// let nextyear = year + 1;
+// let past = 2020 / 2;
+// let future = year * 2;
+
+// ////booleans
+// let trueStatement = false;
+
+
+// ///// Null and undefined
+// let richard;
+// console.log(richard)
+// console.log(jaiyu)
+
+
+
+//// boolean logic
+// let age = 17
+// let boaSquared = 'Bao  Bao'
+// console.log(boaSquared === "Bao Bao")
+// console.log(age == "17")
+
+// let age = 17;
+// if(age < 18) {
+//     console.log("you re a child");
+// } else if (age == 18) {
+//     console.log(happy birthday);
+// } else {
+//     console.log("you are over 18")
+// }
+//red,green, yellow
+let light = "green";
+if(light === "green"){
+    console.log("go")
+} else if (light "yellow") {
+    console.log(caition);
+} else {
+  console.log("stop")
+}
+
+let kristyIntolerant = true;
+let boaBaoIntolerant = false;
+
+if (kristyIntolerant ===true && boaBaoIntolerant === true) {
+        console.log("get almond milk");
+    } else if ( kristyIntolerant === true || boaBaoIntolerant === true){
+        console.log("get both milk");
+    } else{
+        console.log("just regular milk");
+    }
+
+
